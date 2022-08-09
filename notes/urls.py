4ddf14a2905dc15c3833ec18a5ba95852a3fca0a
Harshit Singhal <9414harshit@gmail.com>
2022-08-09 22:46:28 +0530

    update

--- notes/urls.py
+++ notes/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('<int:pk>/',views.detail_view.as_view(), name ='detail'),
     path('new',views.new_notes.as_view(),name='newnote'),
     path('<int:pk>/edit',views.update_notes.as_view(), name ='update'),
-<<<<<<< HEAD
     path('search/',views.search.as_view(), name ='search'),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
     path('login', views.LoginInterfaceView.as_view(), name='login'),
@@ -19,11 +18,6 @@ urlpatterns = [
     path("<int:pk>/comment", views.comment, name="comments"),
     path("<int:pk>/share", views.grouping, name = "share"),
 
-=======
-    path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
-    path('login', views.LoginInterfaceView.as_view(), name='login'),
-    path('Signup', views.Signup.as_view(), name='Signup'),
->>>>>>> e99d61e90a78438ba3b3786b93a5b35b2ee8b8c2
 ]
 
 if settings.DEBUG:
